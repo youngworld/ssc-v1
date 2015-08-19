f0a69af07f280d418f1e40a2912a2984be30926f
youngworld <kennethwchu@gmail.com>
2015-08-19 12:59:17 -0400

    enable first choice via jQuery non-array

--- js/main.js
+++ js/main.js
@@ -1,9 +1,54 @@
 $(document).ready(function() {
 
+// Hide all chapters at first //
+
+$('#chapterA').hide();
+$('#chapterB').hide();
+$('#chapterC').hide();
+$('#chapterD').hide();
+
+
+// After making choice, show first chapter //
+
+$('.choiceA').click(function() {
+  $('#chapterA').show();
+  $('.choices').hide();
+});
+
+$('.choiceB').click(function() {
+  $('#chapterB').show();
+  $('.choices').hide();
+});
+
+$('.choiceC').click(function() {
+  $('#chapterC').show();
+  $('.choices').hide();
+});
+
+$('.choiceD').click(function() {
+  $('#chapterD').show();
+  $('.choices').hide();
+});
+
+
+// At end of first chapter, click Next to show next random chapter //
+
+
+
+
+  // generate random number between 2-4
+
+    // if 2,
+
+
+
 $('.video').click(function() {
   $('.active').removeClass('active');
   $(this).addClass('active');
 
 });
+
+
+
 
 });
