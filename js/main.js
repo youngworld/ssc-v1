b17e08fed7c9ef118b270aa90201583b113bfe64
youngworld <kennethwchu@gmail.com>
2015-08-20 16:41:33 -0400

    update javascript with behavior comments

--- js/main.js
+++ js/main.js
@@ -7,40 +7,77 @@ $('#chapterB').hide();
 $('#chapterC').hide();
 $('#chapterD').hide();
 
+// Declare array //
 
-// After making choice, show first chapter //
+var story = [];
 
-$('.choiceA').click(function() {
+// Declare a counter //
+
+var chaptercount = 0;
+
+
+// Start story with user choice of chapters (click 'Choice') //
+
+$('#choiceA').click(function() {
   $('#chapterA').show();
   $('.choices').hide();
+  story.push('#chapterA');
+  chaptercount = 1;
+  console.log(story, story.length);
+  console.log(chaptercount);
 });
 
-$('.choiceB').click(function() {
+$('#choiceB').click(function() {
   $('#chapterB').show();
   $('.choices').hide();
+  story.push('#chapterB');
+  chaptercount = 1;
+  console.log(story, story.length);
+  console.log(chaptercount);
 });
 
-$('.choiceC').click(function() {
+$('#choiceC').click(function() {
   $('#chapterC').show();
   $('.choices').hide();
+  story.push('#chapterC');
+  chaptercount = 1;
+  console.log(story, story.length);
+  console.log(chaptercount);
 });
 
-$('.choiceD').click(function() {
+$('#choiceD').click(function() {
   $('#chapterD').show();
   $('.choices').hide();
+  story.push('#chapterD');
+  chaptercount = 1;
+  console.log(story, story.length);
+  console.log(chaptercount);
 });
 
 
-// At end of first chapter, click Next to show next random chapter //
+// Remaining chapters generate one-by-one at random (click 'Next') //
+
+do {
+
+  // listen for 'Next' click
+  // function to generate random number between 0(included) and 3(excluded)
+
+  function getRandom() {
+    return Math.floor(Math.random() * 3 + 0()
+  }
+
+  // iterate chapter counter
+  // loop until chapter counter reaches 4
+
+}
+
+while (chaptercount < 4);
 
 
 
 
-  // generate random number between 2-4
 
-    // if 2,
-
-
+// Animate video arrangement //
 
 $('.video').click(function() {
   $('.active').removeClass('active');
