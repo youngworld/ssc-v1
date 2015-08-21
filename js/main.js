9d7dbd105514c1a6f2aa15325ed4f39de2e4148b
youngworld <kennethwchu@gmail.com>
2015-08-21 11:53:42 -0400

    add random chapters for choice B

--- js/main.js
+++ js/main.js
@@ -7,71 +7,108 @@ $('#chapterB').hide();
 $('#chapterC').hide();
 $('#chapterD').hide();
 
-// Declare array //
 
-var story = [];
+// Declare variables to associate styled text chapters //
 
-// Declare a counter //
+var chapterA = '<article id="chapterA"><a name="A" class="anchor"></a><h2>Chapter A</h2> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="video1.html" alt="Video">Cum dolores quaerat nemo non eaque excepturi</a>, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p></article>'
+var chapterB = '<article id="chapterB"><a name="B" class="anchor"></a><h2>Chapter B</h2> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="video1.html" alt="Video">Cum dolores quaerat nemo non eaque excepturi</a>, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p></article>'
+var chapterC = '<article id="chapterC"><a name="C" class="anchor"></a><h2>Chapter C</h2> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="video1.html" alt="Video">Cum dolores quaerat nemo non eaque excepturi</a>, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p></article>'
+var chapterD = '<article id="chapterD"><a name="D" class="anchor"></a><h2>Chapter D</h2> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="video1.html" alt="Video">Cum dolores quaerat nemo non eaque excepturi</a>, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p></article>'
 
-var chaptercount = 0;
+var next ='<div class="next">Next</div>'
 
 
-// Start story with user choice of chapters (click 'Choice') //
+// Declare array of styled story chapters in pre-arranged order //
+
+var storyLeft = [chapterA, chapterB, chapterC, chapterD];
+
+
+// Start story with user choice of first chapter //
 
 $('#choiceA').click(function() {
-  $('#chapterA').show();
   $('.choices').hide();
-  story.push('#chapterA');
-  chaptercount = 1;
-  console.log(story, story.length);
-  console.log(chaptercount);
+  storyLeft.splice(0, 1);
+  window.location.hash = "A";
+  $('.story-wrapper').append(chapterA, next);
+  console.log(storyLeft, storyLeft.length);
 });
+
+
 
 $('#choiceB').click(function() {
-  $('#chapterB').show();
   $('.choices').hide();
-  story.push('#chapterB');
-  chaptercount = 1;
-  console.log(story, story.length);
-  console.log(chaptercount);
+  storyLeft.splice(1, 1);
+  $('.story-wrapper').append(chapterB, next);
+  console.log(storyLeft, storyLeft.length);
+  
+
+$('.next').click(function() {
+  var randomChapter = storyLeft[Math.floor(Math.random() * storyLeft.length)];
+  var randomChapterNum = storyLeft.indexOf(randomChapter);
+  storyLeft.splice(randomChapterNum, 1);
+  $('.story-wrapper').append(randomChapter, next);
+  console.log(storyLeft, storyLeft.length);
+  console.log(randomChapterNum);
+
+
+$('.next').click(function() {
+  var randomChapter = storyLeft[Math.floor(Math.random() * storyLeft.length)];
+  var randomChapterNum = storyLeft.indexOf(randomChapter);
+  storyLeft.splice(randomChapterNum, 1);
+  $('.story-wrapper').append(randomChapter, next);
+  console.log(storyLeft, storyLeft.length);
+  console.log(randomChapterNum);
+
+
+$('.next').click(function() {
+  var randomChapter = storyLeft[Math.floor(Math.random() * storyLeft.length)];
+  var randomChapterNum = storyLeft.indexOf(randomChapter);
+  storyLeft.splice(randomChapterNum, 1);
+  $('.story-wrapper').append(randomChapter);
+  console.log(storyLeft, storyLeft.length);
+  console.log(randomChapterNum);
+
 });
+});
+});
+});
+
 
 $('#choiceC').click(function() {
-  $('#chapterC').show();
   $('.choices').hide();
-  story.push('#chapterC');
-  chaptercount = 1;
-  console.log(story, story.length);
-  console.log(chaptercount);
+  storyLeft.splice(2, 1);
+  $('.story-wrapper').append(chapterC, next);
+  console.log(storyLeft.length);
 });
+
 
 $('#choiceD').click(function() {
-  $('#chapterD').show();
   $('.choices').hide();
-  story.push('#chapterD');
-  chaptercount = 1;
-  console.log(story, story.length);
-  console.log(chaptercount);
+  storyLeft.splice(3, 1);
+  $('.story-wrapper').append(chapterD, next);
+  console.log(storyLeft.length);
 });
 
 
-// Remaining chapters generate one-by-one at random (click 'Next') //
 
-do {
+// Randomly trigger remaining chapters one-by-one //
 
-  // listen for 'Next' click
-  // function to generate random number between 0(included) and 3(excluded)
 
-  function getRandom() {
-    return Math.floor(Math.random() * 3 + 0()
-  }
+$('.next-chapter').click(function() {
 
-  // iterate chapter counter
-  // loop until chapter counter reaches 4
+    console.log('Hello');
 
-}
 
-while (chaptercount < 4);
+    // var randomChapter = storyLeft[Math.floor(Math.random()*storyLeft.length)];
+    // var randomChapterNum = storyleft.indexOf('randomChapter');
+    // storyLeft.splice (randomChapterNum, 1);
+    // $('.story-container').append(randomChapter);
+    // console.log(storyLeft, storyLeft.length);
+    // console.log(randomChapterNum);
+  });
+
+
+
 
 
 
@@ -83,9 +120,8 @@ $('.video').click(function() {
   $('.active').removeClass('active');
   $(this).addClass('active');
 
-});
 
-
+  });
 
 
 });
