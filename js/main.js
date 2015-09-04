6c5a877735c9c195f58d17e2818e9cf915e2174e
youngworld <kennethwchu@gmail.com>
2015-09-04 10:18:14 -0400

    load video arrangements in order

--- js/main.js
+++ js/main.js
@@ -8,12 +8,27 @@ var chapterB = '<article id="chapterB"><a name="B" class="anchor"></a><h2>Chapte
 var chapterC = '<article id="chapterC"><a name="C" class="anchor"></a><h2>Chapter C</h2> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="video1.html" alt="Video">Cum dolores quaerat nemo non eaque excepturi</a>, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p></article>'
 var chapterD = '<article id="chapterD"><a name="D" class="anchor"></a><h2>Chapter D</h2> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores quaerat nemo non eaque excepturi, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="video1.html" alt="Video">Cum dolores quaerat nemo non eaque excepturi</a>, dicta veritatis sed! Tempora cupiditate quisquam, aliquam rem quos! Eligendi, exercitationem molestias porro est nobis?</p></article>'
 
-var next ='<div class="next">Next</div>'
+var VA1 = '<div class="video-wrapper"><div class="video active"></div></div>'
+var VA2 = '<div class="video-wrapper"><div class="video"></div><div class="video active"></div></div>'
+var VA3 = '<div class="video-wrapper"><div class="video"></div><div class="video"></div><div class="video active"></div></div>'
+var VA4 = '<div class="video-wrapper"><div class="video"></div><div class="video"></div><div class="video"></div><div class="video active"></div></div>'
+
+
+var next = '<div class="next">OK</div>'
+
+var videoA = '<video width="800" autoplay loop><source src="videos/videoA.mp4" type="video/mp4"/></video>' 
+var videoB = '<video width="800" autoplay loop><source src="videos/videoB.mp4" type="video/mp4"/></video>'
+var videoC = '<video width="800" autoplay loop><source src="videos/videoC.mp4" type="video/mp4"/></video>' 
+var videoD = '<video width="800" autoplay loop><source src="videos/videoD.mp4" type="video/mp4"/></video>' 
+
 
 
 // Declare array of styled story chapters in pre-arranged order //
 
 var storyLeft = [chapterA, chapterB, chapterC, chapterD];
+var videos = [];
+
+
 
 
 // CHOOSE FIRST CHAPTER //
@@ -23,8 +38,9 @@ var storyLeft = [chapterA, chapterB, chapterC, chapterD];
 $('#choiceA').click(function() {
   $('.choices').hide();
   storyLeft.splice(0, 1);
-  $('.story-wrapper').append(chapterA, next);
-  console.log(storyLeft, storyLeft.length);
+  videos.push(videoA);
+  $('.story-wrapper').append(chapterA, next, VA1);
+  $('.video-wrapper div.active').append(videoA)
 });
 
 
@@ -32,9 +48,10 @@ $('#choiceA').click(function() {
 
 $('#choiceB').click(function() {
   $('.choices').hide();
+  videos.push(videoB);
   storyLeft.splice(1, 1);
-  $('.story-wrapper').append(chapterB, next);
-  console.log(storyLeft, storyLeft.length);
+  $('.story-wrapper').append(chapterB, next, VA1);
+  $('.video-wrapper div.active').append(videoB)
 });
 
 
@@ -42,46 +59,112 @@ $('#choiceB').click(function() {
 
 $('#choiceC').click(function() {
   $('.choices').hide();
+  videos.push(videoC);
   storyLeft.splice(2, 1);
-  $('.story-wrapper').append(chapterC, next);
-  console.log(storyLeft.length);
+  $('.story-wrapper').append(chapterC, next, VA1);
+  $('.video-wrapper div.active').append(videoC)
+
 });
 
 // Start D // 
 
 $('#choiceD').click(function() {
   $('.choices').hide();
+  videos.push(videoD);
   storyLeft.splice(3, 1);
-  $('.story-wrapper').append(chapterD, next);
+  $('.story-wrapper').append(chapterD, next, VA1);
+  $('.video-wrapper div.active').append(videoB)
+
   console.log(storyLeft.length);
 });
+
 
 
 
 // RANDOM REMAINING CHAPTERS //
 
 
+
 $('body').on('click', '.next', function() {
 
     var randomChapter = storyLeft[Math.floor(Math.random()*storyLeft.length)];
-    var randomChapterNum = storyLeft.indexOf(randomChapter);
-    storyLeft.splice (randomChapterNum, 1);
-    $('.story-wrapper').append(randomChapter, next);
+    var randomChapterNum = storyLeft.indexOf(randomChapter);  
+    
 
-    console.log(storyLeft, storyLeft.length);
-    console.log(randomChapterNum);
+console.log(randomChapter);
 
- });
+    switch (randomChapter) {
+      
+
+      case chapterA:
+        console.log("chapterA")
+        videos.push(videoA);
+        break;
+
+      case chapterB:
+       console.log("chapterA")
+        videos.push(videoB);
+        break;
+
+      case chapterC:
+       console.log("chapterA")
+        videos.push(videoC);
+        break;
+
+      case chapterD:
+       console.log("chapterA")
+        videos.push(videoD);
+        break;
+    }
 
 
 
-// ANIMATE VIDEO ARRANGEMENTS //
+         
+          if (storyLeft.length === 3) {
+            $('.story-wrapper').append(randomChapter, next, VA2);  
+            storyLeft.splice (randomChapterNum, 1);
+            attachvideo ();  
 
-$('.video').click(function() {
-  $('.active').removeClass('active');
-  $(this).addClass('active');
+            $('.video').click(function() {
+            $('.active').removeClass('active');
+            $(this).addClass('active');
 
-  });
+            });
 
+
+
+          } else if (storyLeft.length === 2) {
+            $('.story-wrapper').append(randomChapter, next, VA3);
+            storyLeft.splice (randomChapterNum, 1);
+            attachvideo (); 
+             
+            $('.video').click(function() {
+            $('.active').removeClass('active');
+            $(this).addClass('active');
+            });
+         
+
+
+          } else  {
+            $('.story-wrapper').append(randomChapter, VA4);
+            storyLeft.splice (randomChapterNum, 1);
+            attachvideo (); 
+
+            $('.video').click(function() {
+            $('.active').removeClass('active');
+            $(this).addClass('active');
+            });
+
+          }
+      
+});
+
+    function attachvideo () {
+            $('.video-wrapper:last div').each(function(i){
+              console.log(videos[i]);
+              $(this).append(videos[i]);
+            });
+
+          }
 
 });
